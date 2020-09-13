b8b06607961a3fef93dce5b7fc4c5a9b4801b9c5
Ayush Gupta <721ayush@gmail.com>
2020-09-13 11:57:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
+/*
+ *   Copyright (c) 2020 
+ *   All rights reserved.
+ */
+
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './index.css'
+import App from './App;
 
-export default App;
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
