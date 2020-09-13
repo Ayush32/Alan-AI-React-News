ce9059ce93e01bd13e216086bf12dc3376599d92
Ayush Gupta <721ayush@gmail.com>
2020-09-13 17:42:54 +0530

    new file

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@
  *   Copyright (c) 2020
  *   All rights reserved.
  */
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 
 const alanKey =
   "c6b9f6c5e7a6d56e722fadaf02b9a97a2e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App = () => {
+  const [newsArticles, setNewsArticles] = useState([]);
   useEffect(() => {
     alanBtn({
       key: alanKey,
       onCommand: ({ command, articles }) => {
         if (command === "newHeadlines") {
-          console.log(articles);
+          setNewsArticles(articles);
         }
       },
     });
